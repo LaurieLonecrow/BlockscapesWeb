fa3e6abb37c561fc714d8107cbec5f2a1364a2ce
Laurie Lonecrow <laurieteresa@gmail.com>
2021-06-07 18:17:30 -0400

    Creating modal mobile menu

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 'use strict';
 
 function openNav() {
-  document.getElementById("nav-mobile").style.display = "contents";
+  document.getElementById("nav-mobile").style.display = "flex";
+
 
 }
 
 function closeNav() {
   document.getElementById("nav-mobile").style.display = "none";
+
 }
