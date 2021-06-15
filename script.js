8e55b9d9ed86a297c759da33129a285c0d111418
Laurie Lonecrow <laurieteresa@gmail.com>
2021-06-15 13:56:12 -0400

    Duplicates for Github view

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function openNav() {
+  document.getElementById("nav-mobile").style.display = "flex";
+
+
+}
+
+function closeNav() {
+  document.getElementById("nav-mobile").style.display = "none";
+
+}
