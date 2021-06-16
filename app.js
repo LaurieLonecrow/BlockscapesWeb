a595a7cd3440aa8e5808cc1f138be5a85bf3cbe2
Laurie Lonecrow <laurieteresa@gmail.com>
2021-06-16 17:18:25 -0400

    corrected script.js path

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
 
 app.get("/", function(req, res) {
   res.render("home");
