ede2271548284dfaa43bdd676c7b05af2cf70156
Laurie Lonecrow <laurieteresa@gmail.com>
2021-07-04 15:21:17 -0400

    ja update

--- public/js/script.js
+++ public/js/script.js
@@ -2,11 +2,8 @@
 
 function openNav() {
   document.getElementById("nav-mobile").style.display = "flex";
-
-
 }
 
 function closeNav() {
   document.getElementById("nav-mobile").style.display = "none";
-
 }
